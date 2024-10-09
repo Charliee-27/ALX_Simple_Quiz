b2b943b45b9d3a4aedaaaf63eb82683677730140
Charles Mizinga <mizingacharles0@gmail.com>
2024-10-09 21:07:34 +0200

    I added the else statement of the DOM

--- quiz.js
+++ quiz.js
@@ -5,33 +5,7 @@ function checkAnswer() {
     if (userAnswer === correctAnswer) {
         document.getElementById('feedback').textContent = "Correct! Well done.";
     } else {
-        // Handle incorrect answer case
+        feedback.textContent = "That's incorrect. Try again!";
     }
 }
 document.getElementById('submit-answer').addEventListener('click', checkAnswer);
-
-// let number = 0
-
-// if (number > 0) {
-// console.log(number + ' ' + 'is positive')
-// } else if (number < 0) {
-//     console.log(number + ' ' + 'is negative')
-// } else {
-//     console.log(number + ' ' + 'is zero')
-// }
-
-// for (let i=1; i<=10; i++) {
-//     console.log('the initial value is' + ' ' + i);
-// }
-
-// for (let i = 1; i <= 10; i++) {
-//     console.log(i);
-// }
-
-// function sum(num1, num2) {
-//     return num1 + num2;
-//   }
-  
-//   const result = sum(5, 3);
-//   console.log("The sum is:", result);
-  
